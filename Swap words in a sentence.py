3d9606500865667c3f1ef343c27a8b1c7a8b42ca
prayagpadwal <65147413+prayagpadwal@users.noreply.github.com>
2022-10-27 22:59:41 -0700

    Add files via upload

---
+++ Swap words in a sentence.py
@@ -0,0 +1,19 @@
+gas_const = 8.3144621
+
+''' Your solution goes here '''
+
+def compute_gas_volume(gas_pressure, gas_temperature, gas_moles):
+    
+    # V = (nRT)/P
+    
+    gas_volume = (gas_moles * gas_const * gas_temperature) / gas_pressure
+
+    return(gas_volume)
+    
+gas_pressure = float(input())
+gas_moles = float(input())
+gas_temperature = float(input())
+gas_volume = 0.0
+
+gas_volume = compute_gas_volume(gas_pressure, gas_temperature, gas_moles)
+print('Gas volume:', gas_volume, 'm^3')
